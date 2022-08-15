cf4cf3f75385c79e34841e0edc4aa4d00a7e8d0c
Eduardo Melo <nascimentomeloedu@gmail.com>
2022-08-15 16:38:29 -0300

    bugfix: Remove next-pwa dependency

--- next.config.js
+++ next.config.js
@@ -3,16 +3,3 @@ const nextConfig = {
    reactStrictMode: true,
    swcMinify: true,
 };
-
-module.exports = nextConfig;
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require("next-pwa");
-const enviroment = process.env.NODE_ENV === "production";
-
-module.exports = withPWA({
-   pwa: {
-      dest: "public",
-      disable: !enviroment,
-   },
-});
